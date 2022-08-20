1cd1c021de38e560273a2b5169e59cf9f908a4a5
AntonioSilvaDev <>
2022-08-20 11:25:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Hoje dia 17/08/22 estou começando a codar pela primeira vez#
+nome1 = "Antonio"
+
+print(nome1)
+
+nome2 = " Bezerra"
+
+print(nome1+nome2)
+
+nome3 = " da"
+
+nome4 = " Silva"
+
+print(nome1+nome2+nome3+nome4)
